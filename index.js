3cb1b571b409da2e966a38f798f36f87737a75ad
MarionR6 <marionrenuy@gmail.com>
2023-12-22 11:37:38 +0100

    test 2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const routes = require("./routes");
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://etoile-sucree-front.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Credentials", true);
     next();
